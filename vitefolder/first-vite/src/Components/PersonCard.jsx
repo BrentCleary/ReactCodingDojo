212cf69e6f67797ab1fff9a6dcd1e3fc8fefae1c
Brent Cleary <brent.cleary@gmail.com>
2023-10-17 17:30:37 -0700

    Updated Click Count

--- vitefolder/first-vite/src/Components/PersonCard.jsx
+++ vitefolder/first-vite/src/Components/PersonCard.jsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
+
 
 const PersonCard = (props) => {
 
+  const [age, setAge] = useState(props.age);
+
+  const handleClick = () => {
+    setAge( age  + 1 );
+  }
+
   return (
+    <>
+      <div>
+          <h1 style={{color:"red"}}>{props.firstName}</h1>
+          <p>Age: { age } </p>
+          <p>Hair Color: {props.hairColor}</p>
+      </div>
 
-
-    <div>
-        <h1 style={{color:"red"}}>{props.firstName}</h1>
-        <p>Age: {props.age}</p>
-        <p>Hair Color: {props.hairColor}</p>
-    </div>
-  );
+      <div>
+        <button onClick={ handleClick }>Update Birthday</button>
+      </div>
+    </>
+);
 }
 
 export default PersonCard;
